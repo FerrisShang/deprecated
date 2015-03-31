4ebab1130127b2497e30510d550d79045bd57170
ShangFeng <feng.shang@ingenic.com>
2015-03-31 16:45:06 -0400

    add udp control halley board led code

---
+++ c_code/linux/user/udp_led_ctrl/protocol.h
@@ -0,0 +1,21 @@
+
+#define PREAMBLE_POS 0
+#define LEN_POS 2
+#define VERSION_POS 4
+#define CMD_POS 8
+#define DATA_POS 10
+
+#define PREAMBLE {0x4A, 0x5A}
+#define DEVICE_ID {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, \
+	0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}
+#define VERSION {0x00, 0x00, 0x01 ,0x01}
+
+#define CMD_LOGIN			 0x01
+#define CMD_ACK_LOGIN		 0x02
+#define CMD_SET_STATUS		 0x03
+#define CMD_GET_STATUS		 0x04
+#define CMD_REPORT_STATUS	 0x05
+
+#define HEARTBEAT_INTV		 12
+int str_login(char *buf);
+int str_report(char *buf);
