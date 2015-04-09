164a341d79be78aa9db71d97b8cc2bf81230fab5
ShangFeng <feng.shang@ingenic.com>
2015-04-09 17:04:57 -0400

    add fuction @ test_mic_hp

--- c_code/linux/user/test_mic_hp/test_mic_hp.c
+++ c_code/linux/user/test_mic_hp/test_mic_hp.c
@@ -5,48 +5,89 @@
 #include <sys/ioctl.h>
 #include <stdlib.h>
 #include <linux/soundcard.h>
+#include <string.h>
 
-#define LENGTH 1
-#define RATE 44200
+#define LENGTH 2
+#define RATE 16000
 #define SIZE 16
-#define CHANNELS 1
+#define CHANNELS 2
 #define AUDIO_DEV "/dev/jz-dmic"
 #define BUF_SIZE (int)(1.0*LENGTH*RATE*SIZE*CHANNELS/8.0)
 
-#define VOL_LEVEL 0
+#define MIXER_DEV "/dev/snd/mixer"
+#define VOL_LEVEL 10
 
-unsigned char buf[BUF_SIZE];
 int main(int argc, char* argv[])
 {
+	char *buf;
+	char audio_dev[32];
+	char mixer_dev[32];
+	int rate, buf_size, vol_level;
 	int fd_read, fd_write, fd_vol;
-	int arg;
+	int arg, i;
 
-	
-	printf("open "AUDIO_DEV"\n");
-	fd_read = open(AUDIO_DEV, O_RDONLY);
-	arg = SIZE;
-	ioctl(fd_read, SOUND_PCM_WRITE_BITS, &arg);
-	arg = CHANNELS;
-	ioctl(fd_read, SOUND_PCM_WRITE_CHANNELS, &arg);
-	arg = RATE;
-	ioctl(fd_read, SOUND_PCM_WRITE_RATE, &arg);
+	if(argc >= 2)
+		strcpy(audio_dev, argv[1]);
+	else
+		strcpy(audio_dev, AUDIO_DEV);
+	if(argc >= 3)
+		rate = atoi(argv[2]);
+	else
+		rate = RATE;
+	if(argc >= 5){
+		strcpy(mixer_dev, argv[3]);
+		vol_level = atoi(argv[4]);
+	}else{
+		strcpy(mixer_dev, MIXER_DEV);
+		vol_level = VOL_LEVEL;
+	}
+	//set vol
+	if((fd_vol = open(mixer_dev, O_RDONLY)) < 0){
+		perror("open mixer file failed");
+	}else{
+		ioctl(fd_vol, MIXER_WRITE(0), &vol_level);
+		close(fd_vol);
+	}
+
+	buf_size  = (int)(1.0*LENGTH*rate*SIZE*CHANNELS/8.0);
+	buf = malloc(buf_size);
+	if(buf < 0){
+		perror("malloc failed");
+		exit(1);
+	}
+	if((fd_write = open(audio_dev,O_WRONLY)) < 0){
+		perror("open dsp file for writing failed");
+	}else{
+		arg = SIZE;
+		ioctl(fd_write , SOUND_PCM_WRITE_BITS, &arg);
+		arg = CHANNELS;
+		ioctl(fd_write , SOUND_PCM_WRITE_CHANNELS, &arg);
+		arg = rate;
+		ioctl(fd_write , SOUND_PCM_WRITE_RATE, &arg);
+	}
+	if((fd_read = open(audio_dev,O_RDONLY)) < 0){
+		perror("open dsp file for reading failed");
+		for(i=0;i<buf_size;i++){
+			buf[i] = rand();
+		}
+	}
 
 	while(1){
-		int i,all_zero_flag;
-		int sum;
-		read(fd_read,buf,BUF_SIZE);
-		sum = 0;
-		all_zero_flag = 1;
-		for(i=0;i<BUF_SIZE;i++){
-			if(buf[i] != 0)
-				all_zero_flag = 0;
-			if(buf[i]>=0)
+		if(fd_read < 0 && fd_write < 0)exit(1);
+		if(fd_read > 0){
+			int sum = 0;
+			printf("recording..\n");
+			read(fd_read, buf, buf_size);
+			for(i=0;i<buf_size;i++){
 				sum += buf[i];
-			else
-				sum -= buf[i];
+			}
+			if(sum = 0){
+				printf("warning ======================> no sound detected.\n");
+			}
 		}
-		if(all_zero_flag)
-			printf("no voice input..  ");
-		printf("ave = %d\n", sum/BUF_SIZE);
+		if(fd_write > 0){
+			printf("playing..\n");
+			write(fd_write,buf,buf_size);
+		}
 	}
 }
