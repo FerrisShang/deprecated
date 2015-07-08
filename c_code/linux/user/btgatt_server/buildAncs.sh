f0b78a593e0642450c95110611ee1aadeb40e217
ShangFeng <feng.shang@ingenic.com>
2015-07-08 05:21:55 -0400

    add ancs test code,connect success, but authentication failed

---
+++ c_code/linux/user/btgatt_server/buildAncs.sh
@@ -0,0 +1,33 @@
+OUTPUT_NAME=ancs_test
+COMPILE_CROSS=mips-linux-gnu
+CURRENT=`pwd`
+PREFIX=/home/fshang/f/bluez/output/app/usr
+CFLAG="-I. \
+	   -fdata-sections -ffunction-sections
+	   "
+LDFLAG="-L$PREFIX/lib \
+		-Wl,--gc-sections
+		"
+LIBS="-lrt \
+	  -lpthread \
+	  "
+SRC="tools/ancs_test.c \
+	 src/shared/gatt-client.c \
+	 src/shared/gatt-helpers.c \
+	 src/shared/mainloop.c \
+	 src/shared/gatt-db.c \
+	 src/shared/gatt-server.c \
+	 src/shared/att.c \
+	 src/shared/queue.c \
+	 src/shared/util.c \
+	 src/shared/crypto.c \
+	 src/shared/io-mainloop.c \
+	 src/shared/timeout-mainloop.c \
+	 lib/bluetooth.c \
+	 lib/uuid.c \
+	 lib/hci.c \
+	 "
+$COMPILE_CROSS-gcc $CFLAG $LDFLAG $INC $LIBS $SRC -o $OUTPUT_NAME
+$COMPILE_CROSS-strip $OUTPUT_NAME
+#adb push $OUTPUT_NAME /
+scp $OUTPUT_NAME user@192.168.3.92:~/fshang/adb_halley/bt_tool/
