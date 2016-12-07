2e8d5889febe32c46992f3b59f662e393856ff44
ShangFeng <feng.shang@ingenic.com>
2016-12-07 01:47:00 -0500

    add btgatt_client

---
+++ c_code/linux/user/bluetooth/btgatt_client/build.sh
@@ -0,0 +1,22 @@
+echo "building btgatt-client"
+CC=mips-linux-gnu-gcc
+SOURCE_FOLDER_GATTC=.
+SOURCES_GATTC="\
+$SOURCE_FOLDER_GATTC/tools/btgatt-client.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/gatt-client.c
+	   $SOURCE_FOLDER_GATTC/src/shared/gatt-helpers.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/mainloop.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/gatt-db.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/gatt-server.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/att.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/queue.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/util.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/crypto.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/io-mainloop.c \
+	   $SOURCE_FOLDER_GATTC/src/shared/timeout-mainloop.c \
+	   $SOURCE_FOLDER_GATTC/lib/bluetooth.c \
+	   $SOURCE_FOLDER_GATTC/lib/uuid.c \
+	   $SOURCE_FOLDER_GATTC/lib/hci.c \
+	   "
+CFLAGS_GATTC=-I$SOURCE_FOLDER_GATTC
+$CC $SOURCES_GATTC $CFLAGS_GATTC -o ./btgatt-client
