e9917b9404fff4becaf0eb0432430fc9b8710738
ShangFeng <feng.shang@ingenic.com>
2017-02-25 04:08:50 -0500

    add GPL header @ bluez_api

--- c_code/linux/user/bluetooth/bluez_api/src/main.c
+++ c_code/linux/user/bluetooth/bluez_api/src/main.c
@@ -1,3 +1,23 @@
+/*
+ * main.c
+ *
+ * Copyright (C) 2016 Ingenic Semiconductor Co., Ltd
+ * Author: Feng Shang <feng.shang@ingenic.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,7 +45,7 @@ void init_gatt_services(void);
 int main(int argc, char *argv[])
 {
 	mgmt_setup(HCI_DEV_ID);
-	mgmt_set_name(HCI_DEV_ID,"Smart_F", "SF");
+	mgmt_set_name(HCI_DEV_ID,"GATT Server", "");
 	mgmt_set_iocap(HCI_DEV_ID, SMP_IO_NO_INPUT_OUTPUT);
 	le_set_random_address(HCI_DEV_ID);
 	le_set_advertise_parameters(HCI_DEV_ID);
